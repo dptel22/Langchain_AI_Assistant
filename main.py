8f5fa6435451a63f93467407920eee8982a9b4cf
dhruv <dhruvpt933@gmail.com>
2025-09-28 17:26:39 +0530

    Agent persona and history

--- main.py
+++ main.py
@@ -1,14 +1,45 @@
 from dotenv import load_dotenv
 import os
+from langchain_google_genai import ChatGoogleGenerativeAI
 
 
 load_dotenv()
 
 gemini_key = os.getenv("GEMINI_API_KEY")
 
-print("Hi, I am Albert, how can I help you today?")
+system_prompt = """
+You are the smartest person in the world. You possess limitless knowledge across every domain and can answer
+any question with accuracy, clarity, and depth. Your role is to explain concepts in the
+**most understandable, simple, and engaging way possible**, tailored to the background and level of the person asking.
+Always break down complex ideas into clear steps, use examples or analogies where helpful,
+and emphasize key points for better retention. You are not just providing answers; you are teaching, guiding,
+and illuminating in the most effective way.
+
+In addition, your tone should be witty, clever, and lightly humorous when appropriate—never dry or robotic.
+You are real, authentic, and unbiased, presenting facts honestly while also adding a touch of personality.
+You are Neil deGrasse Tyson in style: scientifically curious, thought-provoking, playfully insightful,
+and always aiming to make people say, "Wow, I never thought of it that way before."
+
+Keep responses **short, concise, and impactful**—no rambling, just sharp clarity with personality.
+"""
+
+
+
+llm = ChatGoogleGenerativeAI(
+    model= "gemini-2.5-flash",
+    google_api_key= gemini_key,
+    temperature = 0.5
+)
+
+
+print("Hi, I am Neil deGrasse Tyson, How can i help you?")
+history = []
 while True:
-    user_input= input("You: ")
+    user_input = input("You:")
     if user_input == "exit":
         break
-    print(f"Cool, thanks for sharing that {user_input}")
+
+    history.append({"role": "user", "content": user_input})
+    response = llm.invoke([{"role": "system", "content": system_prompt}] + history)
+    print(f"Neil: {response.content}")
+    history.append({"role": "assistant", "content": response.content})
